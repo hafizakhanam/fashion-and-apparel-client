3416bd3997413cce7d910bcee5a862650d79f27e
hafizakhanam <hafiza.khanam0@gmail.com>
2023-10-20 17:14:56 +0600

    product details update

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -9,12 +9,12 @@ const ProductDetails = () => {
             <AdvSlider></AdvSlider>
             <div className="py-40 bg-white">
                 <div className="max-w-[1340px] mx-auto px-4">
+                    <h2 className="text-blue-950 font-bold text-5xl mb-16 text-center">{productName}</h2>
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                         <img className="text-center rounded-lg w-full" src={productImage} alt="Image" />
                         <div className="p-8">
                             <button className="btn border border-white mb-6">Add to Cart</button>
                             <h2 className="card-title text-lg font-semibold text-blue-950">Ratings: <span className="text-sm">{rating}</span></h2>
-                            <h2 className="card-title text-lg font-semibold text-blue-950">Name: <span className="text-sm">{productName}</span></h2>
                             <h2 className="card-title text-lg font-semibold text-blue-950">Brand: <span className="text-sm">{brandName}</span></h2>
                             <h2 className="card-title text-lg font-semibold text-blue-950">Type: <span className="text-sm">{productType}</span></h2>
                             <h2 className="card-title text-lg font-semibold text-blue-950">Price: <span className="text-sm">{productPrice}</span></h2>
